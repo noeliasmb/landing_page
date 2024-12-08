6d43d56df97898848dfea6e4fe2cda329edcf16f
noeliasmb <noeliasanchezpsicologa@gmail.com>
2024-12-08 22:52:14 +0100

    added footer

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrls: ['./footer.component.scss'] // Fixed the typo from "styleUrl" to "styleUrls"
 })
 export class FooterComponent {
+  currentYear: number;
 
+  constructor() {
+    this.currentYear = new Date().getFullYear(); // Dynamically sets the current year
+  }
 }
